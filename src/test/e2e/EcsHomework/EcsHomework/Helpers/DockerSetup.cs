078752f3857b95e44879c425eea2618c72a712e3
Adam Holcombe <Adam.Holcombe@gmail.com>
2020-02-02 14:20:00 +0000

    Added some docker support

---
+++ src/test/e2e/EcsHomework/EcsHomework/Helpers/DockerSetup.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+
+namespace EcsHomework.Helpers
+{
+    class DockerSetup
+    {
+
+        public static void StartDocker()
+        {
+            StopDocker();
+
+            var processInfo = new ProcessStartInfo("docker", $"run -it -p 3000:3000 ecsd-tech-test:latest");
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardError = true;
+
+            int exitCode;
+            using (var process = new Process())
+            {
+                process.StartInfo = processInfo;
+                // process.OutputDataReceived += new DataReceivedEventHandler("C:/temp.txt");
+                //  process.ErrorDataReceived += new DataReceivedEventHandler(logOrWhatever());
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit(10000);
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+
+                exitCode = process.ExitCode;
+                process.Close();
+            }
+
+        }
+
+        public static void StopDocker()
+        {
+            var processInfo = new ProcessStartInfo("docker", $"stop $(docker ps -aq)");
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardError = true;
+
+            int exitCode;
+            using (var process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit(10000);
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+
+                exitCode = process.ExitCode;
+                process.Close();
+
+            }
+
+            RemoveContainer();
+        }
+
+        public static void RemoveContainer()
+        {
+            var processInfo = new ProcessStartInfo("docker", $"rm $(docker ps -aq)");
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardError = true;
+
+            int exitCode;
+            using (var process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit(10000);
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+
+                exitCode = process.ExitCode;
+                process.Close();
+
+            }
+
+        }
+
+
+
+    }
+}
--- src/test/e2e/EcsHomework/EcsHomework/Hooks/Hooks.cs
+++ src/test/e2e/EcsHomework/EcsHomework/Hooks/Hooks.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TechTalk.SpecFlow;
+using System.Collections.ObjectModel;
+
 
 namespace EcsHomework.Hooks
 {
@@ -12,16 +14,33 @@ namespace EcsHomework.Hooks
     {
         // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
 
+       [BeforeTestRun]
+       public static void StartDocker()
+        {
+            DockerSetup.StartDocker();
+
+        }
+
+
         [BeforeScenario]
         public void BeforeScenario()
         {
             new TestDriver().SetDriver();
         }
-
+        
+    
         [AfterScenario]
         public void AfterScenario()
         {
           new TestDriver().QuitDriver();
         }
+
+        [AfterTestRun]
+        public static void StopDocker()
+        {
+            DockerSetup.StopDocker();
+
+        }
+       
     }
 }
